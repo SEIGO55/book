fb0a651efbba36940188041bdc5433a83de5cc15
SEIGO55 <145002301+SEIGO55@users.noreply.github.com>
2024-10-10 18:13:55 +0900

    Update and rename section1.py to other_script.py

---
+++ other_script.py
@@ -0,0 +1,8 @@
+# other_script.py
+
+def get_additional_text():
+    additional_text = '''
+    4章 データの前処理 [書式1:  章タイトル]
+    データの前処理について説明します。[書式4:  文]
+    '''
+    return additional_text
--- section1.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-def show():
-    st.title("第1章　業務と機械学習プロジェクト")
