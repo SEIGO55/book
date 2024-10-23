6d96956ebc192f7cb3cf8781abe8dc8f4f918f0c
SEIGO55 <145002301+SEIGO55@users.noreply.github.com>
2024-10-23 09:48:21 +0900

    Create page2.py

---
+++ page2.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def display_page():
+    st.markdown("# Page 2")
+    st.markdown("こちらは Page 2 の内容です。")
+    st.markdown("Markdown形式で表記されています。")
+
+# 関数を呼び出して内容を表示
+display_page()
