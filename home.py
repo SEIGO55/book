e2c3f18c55489baac6f84e50e7fbb01046abd65c
SEIGO55 <145002301+SEIGO55@users.noreply.github.com>
2024-10-23 10:44:43 +0900

    Update home.py

--- home.py
+++ home.py
@@ -1,17 +1,6 @@
 import streamlit as st
 
-# サイドバーに選択ボックスを表示
-page = st.sidebar.selectbox("ページを選択", ["home", "page2", "page3"])
+st.title("Home Page")
+st.write("これはホームページです。")
 
-# ページに応じてファイルをインポート
-if page == "home":
-    st.title("Home Page")
-    st.write("これはホームページです。")
-
-elif page == "page2":
-    # page2.pyの内容を読み込む
-    import page2
-
-elif page == "page3":
-    # page3.pyの内容を読み込む
-    import page3
+import page3
