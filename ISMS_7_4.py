c988ea4a6705dc311e0603c3a370f8215f8e4e5d
SEIGO55 <145002301+SEIGO55@users.noreply.github.com>
2024-10-29 15:35:10 +0900

    Update ISMS_7_4.py

--- ISMS_7_4.py
+++ ISMS_7_4.py
@@ -12,7 +12,6 @@ def display_page():
           この管理策では、組織が物理的な敷地を継続的に監視することが求められています。目的は、敷地内での監視を通じて、怪しい行動を抑止し、検知することです。
           </p>
         </details>
-    
         <details>
            <summary style='font-size:16px; font-weight: bold;'>2. 物理的セキュリティ監視の手法</summary>
           <p>
@@ -21,7 +20,6 @@ def display_page():
           監視システムは、要塞のように敷地全体を監視し、組織のセキュリティの要として機能します。
           </p>
         </details>
-    
         <details>
           <summary>3. 監視システムの主要要素</summary>
           <p><strong>1. CCTV（閉回路テレビ）</strong></p>
@@ -36,7 +34,6 @@ def display_page():
           これにより、不審者を抑止し、セキュリティチームに潜在的な脅威を通知します。
           </p>
         </details>
-    
         <details>
           <summary>4. 監視システムの設計とセキュリティ</summary>
           <p>
@@ -44,7 +41,6 @@ def display_page():
           カメラやセンサーの位置が公開されると、不審者がそれらを回避する手がかりとなる可能性があるため、できるだけ隠し、選ばれた人のみが知るようにします。
           </p>
         </details>
-    
         <details>
           <summary>5. 監視システムの保護</summary>
           <p>
@@ -52,7 +48,6 @@ def display_page():
           アラームシステムのコントロールパネルは、緊急時にアクセス可能でありながら、改ざんから守られるように設計されるべきです。
           </p>
         </details>
-    
         <details>
           <summary>6. プライバシー法とデータ保護規制の遵守</summary>
           <p>
