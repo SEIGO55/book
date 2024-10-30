f7003b214f96eafab1616bd4319849b71679b21b
SEIGO55 <145002301+SEIGO55@users.noreply.github.com>
2024-10-30 13:28:16 +0900

    Update home.py

--- home.py
+++ home.py
@@ -3,8 +3,19 @@ import streamlit as st
 st.title("Home Page")
 st.write("これはホームページです。")
 
-import section1
-section1.display_page()
+import os
+from pdf2image import convert_from_path
 
-import section2
-section2.display_page()
+# 保存用のディレクトリを作成
+output_dir = "./slides"
+os.makedirs(output_dir, exist_ok=True)
+
+# PDFファイルのパスを指定
+pdf_path = "./DX-9.pdf"
+
+# PDFを画像に変換して、スライドごとに指定したフォルダに保存
+slides = convert_from_path(pdf_path)
+for i, slide in enumerate(slides):
+    slide.save(f"{output_dir}/slide_{i+1}.png", "PNG")  # 各ページを指定フォルダに保存
+
+
